f21c817653bbda9d2fb741799d97fe26bbf6834c
mjyun01 <87511647+mjyun01@users.noreply.github.com>
2024-03-18 23:06:02 +0900

    Update get_fft_time.py

--- week9/get_fft_time.py
+++ week9/get_fft_time.py
@@ -1,9 +1,16 @@
 import numpy as np
 import scipy.io as sio
 import scipy.io.wavfile
-from scipy import fftpack
+import matplotlib.pyplot as plt
+import argparse 
 
-samplerate, data = sio.wavfile.read('test.wav')
+parser = argparse.ArgumentParser()
+parser.add_argument('--filename', required=False, default='filename.wav')
+args = parser.parse_args()
+
+print("drawing plot for", args.filename)
+
+samplerate, data = sio.wavfile.read(args.filename)
 fftsize = len(data)
 data_fft = fftpack.fft(data, fftsize)
 
