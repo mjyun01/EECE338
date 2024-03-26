a318313ee47459461e848cd74ce7bd95e12cdd44
mjyun01 <87511647+mjyun01@users.noreply.github.com>
2024-03-26 15:16:01 +0900

    Update draw_waveform.py

--- week9/draw_waveform.py
+++ week9/draw_waveform.py
@@ -13,7 +13,6 @@ print("drawing plot for", args.filename)
 samplerate, data = sio.wavfile.read(args.filename)
 times = np.arange(len(data))/float(samplerate)		# (0:length(data)-1)/samplerate
 
-plt.title("ploting for", args.filename) 
 plt.fill_between(times, data)		#fill the area between two horizontal curves(y=data and y=0) 
 plt.xlim(times[0], times[-1])		#set x-axis limit
 plt.xlabel('times (s)')
